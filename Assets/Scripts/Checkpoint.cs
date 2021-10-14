fb6e10408bc0dc2894f2008268f34f2d7c3e5ab4
VíctorLópez <victor_lopez_bc3dv1mb2021@cev.com>
2021-10-14 19:02:00 +0200

    CP

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -23,6 +23,13 @@ public class Checkpoint : MonoBehaviour
         {
             GameManager.instance.SetSpawnPoint(transform.position);
 
+            Checkpoint[] allCP = FindObjectsOfType<Checkpoint>();
+            for( int i = 0; i< allCP.Length; i++)
+            {
+                allCP[i].cpOFF.SetActive(true);
+                allCP[i].cpON.SetActive(false);
+            }
+
             cpOFF.SetActive(false);
             cpON.SetActive(true);
         }
