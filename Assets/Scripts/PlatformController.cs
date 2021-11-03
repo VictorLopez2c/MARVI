6438044fd291409c8dd4c4d77494be82b1769249
VictorLopez2c <victor_lopez_bc3dv1mb2021@cev.com>
2021-11-03 17:15:39 +0100

    Plataformas

--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -7,6 +7,12 @@ public class PlatformController : MonoBehaviour
     public Rigidbody platformRB;
     public Transform[] platformPositions;
     public float platformSpeed;
+
+    private int actualPosition = 0;
+    private int nextPosition = 1;
+
+    public bool moveToTheNext = true;
+    public float waitTime;
   
     void Update()
     {
@@ -15,6 +21,30 @@ public class PlatformController : MonoBehaviour
 
     void MovePlatform()
     {
-        platformRB.MovePosition(Vector3.MoveTowards(platformRB.position, platformPositions[1].position, platformSpeed * Time.deltaTime));
+        if (moveToTheNext)
+        {
+            StopCoroutine(WaitForMove(0));
+            platformRB.MovePosition(Vector3.MoveTowards(platformRB.position, platformPositions[1].position, platformSpeed * Time.deltaTime));
+        }
+        
+
+        if (Vector3.Distance(platformRB.position, platformPositions[nextPosition].position) <= 0)
+        {
+            StartCoroutine(WaitForMove(waitTime));
+            actualPosition = nextPosition;
+            nextPosition++;
+
+            if (nextPosition > platformPositions.Length - 1)
+            {
+                nextPosition = 0;
+            }
+        }
+    }
+
+    IEnumerator WaitForMove(float time)
+    {
+        moveToTheNext = false;
+        yield return new WaitForSeconds(time);
+        moveToTheNext = true;
     }
 }
