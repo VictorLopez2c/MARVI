c1b05d162851f2d869b098b5ff13edf517e9e7b2
Marc Agudo <marc_agudo_bc3dv1mb2021@cev.com>
2022-03-30 17:16:47 +0200

    oro

--- Assets/Scripts/GoldPickup.cs
+++ Assets/Scripts/GoldPickup.cs
@@ -11,11 +11,24 @@ public class GoldPickup : MonoBehaviour
     public GameObject goldEffect;
 
     public AudioSource goldSound;
+    
+    public enum UIElementType { 
+       Jugete,
+       Disco,
+       Olisbo,
+       Calavera
+    }
+
+    public UIElementType type;
+
+    Slider slider;
 
     void Start()
     {
         UIManager.instance.goldImage.enabled = false;//PickUp IMG- Canvas Disabled
+        
     }
+    
 
     
     void Update()
@@ -45,6 +58,34 @@ public class GoldPickup : MonoBehaviour
             GameManager.instance.AddGold(value);
             goldSound.Play();
             Instantiate(goldEffect, transform.position, transform.rotation);
+            
+            switch (type)
+            {
+                case UIElementType.Jugete:
+                    /*slider = GetComponent<Slider>();
+                    slider.value = OroSettings.audioSettings.GetRobot();*/
+                    Oro = 1;
+                    break;
+                case UIElementType.Disco:
+                    /*slider = GetComponent<Slider>();
+                    slider.value = OroSettings.audioSettings.GetDisco();*/
+                    Oro = 2;
+                    break;
+                case UIElementType.Olisbo:
+                    /*slider = GetComponent<Slider>();
+                    slider.value = OroSettings.audioSettings.GetOlisbo();*/
+                    Oro = 3;
+                    break;
+                case UIElementType.Calavera:
+                    /*slider = GetComponent<Slider>();
+                    slider.value = OroSettings.audioSettings.GetCalavera();*/
+                    Oro = 4;
+                    break;
+            }
+            if( Oro == 1)
+            {
+                Oro
+            }
 
         }
     }
@@ -53,7 +94,7 @@ public class GoldPickup : MonoBehaviour
         UIManager.instance.goldImage.enabled = !UIManager.instance.goldImage.enabled;
         yield return new WaitForSeconds(0.5f);
         UIManager.instance.goldImage.enabled = !UIManager.instance.goldImage.enabled;
-        Destroy(gameObject);
+        //Destroy(gameObject);
     }
 
 
---
+++ Assets/Scripts/Opciones/Audio/OroSettings.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OroSettings : MonoBehaviour
+{
+    public static OroSettings audioSettings;
+
+    //[Header("Information - Read Only from inspector")]
+    [SerializeField]
+    private float musicVolume;
+    [SerializeField]
+    private float sfxVolume;
+    [SerializeField]
+    private int robotVolume = 1;
+    [SerializeField]
+    private int olisboVolume = 2;
+    [SerializeField]
+    private int calaveraVolume = 3;
+    [SerializeField]
+    private int discoVolume = 4;
+
+    float musicDefaultVolume=0.7f;
+    float sfxDefaultVolume = 0.9f;
+    public GameObject FxBase;
+
+    //string musicAudioSourcesTag ="Music-AudioSource";
+    //string sfxAudioSourcesTag="SFX-AudioSource";
+
+    string musicVolumeDataName = "music-volume";
+    string sfxVolumeDataName = "sfx-volume";
+
+    List<AudioSource> musicAudioSources;
+    List<AudioSource> sfxAudioSources;
+
+    [SerializeField]
+    private int musicAudioSourcesCount=0;
+    [SerializeField]
+    private int sfxAudioSourcesCount = 0;
+
+    private void Awake()
+    {
+        audioSettings = this;
+        musicAudioSources = new List<AudioSource>();
+        sfxAudioSources = new List<AudioSource>();
+        LoadSavedSettings();
+    }
+
+    void LoadSavedSettings()
+    {
+        musicVolume = PlayerPrefs.GetFloat(musicVolumeDataName,musicDefaultVolume);
+        sfxVolume = PlayerPrefs.GetFloat(sfxVolumeDataName, sfxDefaultVolume);
+
+    }
+
+    public void ChangeMusicVolume(float newVolume)
+    {
+        musicVolume = newVolume;
+        PlayerPrefs.SetFloat(musicVolumeDataName, musicVolume);
+        SetVolumeToAudioSources(musicAudioSources, musicVolume);
+    }
+
+
+    public void ChangSFXVolume(float newVolume)
+    {
+        sfxVolume = newVolume;
+        PlayerPrefs.SetFloat(sfxVolumeDataName, sfxVolume);
+        SetVolumeToAudioSources(sfxAudioSources, sfxVolume);
+        Instantiate(FxBase);
+    }
+
+    void SetVolumeToAudioSources(List<AudioSource> audioSources, float volume)
+    {
+        foreach (AudioSource a in audioSources)
+        {
+            a.volume = volume;
+        }
+    }
+
+
+    public float GetRobot()
+    {
+        return robotVolume;
+    }
+    public float GetOlisbo()
+    {
+        return olisboVolume;
+    }
+    public float GetCalavera()
+    {
+        return calaveraVolume;
+    }
+    public float GetDisco()
+    {
+        return discoVolume;
+    }
+    
+
+    public void AddMeToMusicAudioSources(AudioSource a)
+    {
+        musicAudioSources.Add(a);
+        musicAudioSourcesCount = musicAudioSources.Count;
+    }
+
+    public void RemoveMeFromMusicAudioSources(AudioSource a)
+    {
+        musicAudioSources.Remove(a);
+        musicAudioSourcesCount = musicAudioSources.Count;
+    }
+    public void AddMeToSFXAudioSources(AudioSource a)
+    {
+        sfxAudioSources.Add(a);
+        sfxAudioSourcesCount = sfxAudioSources.Count;
+    }
+
+    public void RemoveMeFromSFXAudioSources(AudioSource a)
+    {
+        sfxAudioSources.Remove(a);
+        sfxAudioSourcesCount = sfxAudioSources.Count;
+    }
+
+
+}
